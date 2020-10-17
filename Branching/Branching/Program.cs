5041a64474f9706255ef2ca05bba8a7599e66f6d
Admin <spirit2914@outlook.com>
2020-10-17 14:32:21 -0500

    Corrected StudentDailyReport

---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main()
+        {
+            int currentTemp = 70;
+            int roomTemp = 70;
+            if(currentTemp == roomTemp)
+            {
+                Console.WriteLine("It is exactly room temperature");
+            }
+            else if (currentTemp > roomTemp)
+            {
+                Console.WriteLine("It is warmer than room temperature");
+            }
+            else if (currentTemp < roomTemp)
+            {
+                Console.WriteLine("Room temperature is warmer than current temperature");
+            }
+            else 
+            {
+                Console.WriteLine("It is not exactly room temperature");
+            }
+
+            string result = roomTemp > currentTemp ? "Room temperature is warmer than current temperature" : "Current temperature is warmer than room temperature";
+            Console.WriteLine(result);
+            string comparisonResult = roomTemp == currentTemp ? "It is room temp" : "It is not room temp";
+            Console.WriteLine(comparisonResult);
+
+            int roomTemp2 = 70;
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi, " + name + ", what is the temperature where you are?");
+            int currentTemp2 = Convert.ToInt32(Console.ReadLine());
+            if (currentTemp2 == roomTemp2)
+            {
+                Console.WriteLine("It is exactly room temperature.");
+            }
+            else if (currentTemp2 > roomTemp2)
+            {
+                Console.WriteLine("It is warmer than room temperature.");
+            }
+            else if (currentTemp2 < roomTemp2)
+            {
+                Console.WriteLine("It is colder than room temperature.");
+            }
+            else
+            {
+                Console.WriteLine("It is not exactly room temperature.");
+            }
+
+            Console.WriteLine("What is your favorite number?");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+            String result3 = favNum == 12 ? "You have an awesome number" : "You did not have an awesome number";
+            Console.WriteLine(result3);
+            Console.ReadLine();
+        }
+                
+    }
+}
--- StudentDailyReport/StudentDailyReport/Program.cs
+++ StudentDailyReport/StudentDailyReport/Program.cs
@@ -17,15 +17,15 @@ namespace StudentDailyReport
             Console.WriteLine("What course are you on?");
             string course = Console.ReadLine();
             Console.WriteLine("What page number?");
-            string page = Console.ReadLine();
+            int page = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Do you need help with anything? Please answer \"true \" or \"false\".");
-            string answer = Console.ReadLine();
+            bool answer = Convert.ToBoolean(Console.ReadLine());
             Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
             string share = Console.ReadLine();
             Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
             string feedback = Console.ReadLine();
             Console.WriteLine("How many hours did you study today?");
-            string hours = Console.ReadLine();
+            int hours = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
             Console.ReadLine();
         }
