6f35577be367675ded6f8309c3c94da9ff47661c
Admin <spirit2914@outlook.com>
2020-10-17 18:30:44 -0500

    Added ArrayAndListAssignment

---
+++ ArrayAndListAssignment/ArrayAndListAssignment/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAndListAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArray = new string[] {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+            Console.WriteLine("Please choose a number from 0 - 9.");
+            string answer = Console.ReadLine();
+            int index = Convert.ToInt32(answer);
+            if (index < 0 || index > 9)
+            {
+                Console.WriteLine("The data does not exist");
+             
+            }
+            else
+            {
+                Console.WriteLine("Here is what you chose: " + stringArray[index]);
+            }
+            
+
+            int[] intArray = new int[] { 32, 62, 50, 25, 100, 2560, 2552 };
+            Console.WriteLine("Please choose a number from 0 - 6.");
+            string intAnswer = Console.ReadLine();
+            int intIndex = Convert.ToInt32(intAnswer);
+            if (intIndex < 0 || intIndex > 6)
+            {
+                Console.WriteLine("The data does not exist");
+            }
+            else
+            {
+                Console.WriteLine("Here is the number you chose: " + intArray[intIndex]);
+            }
+           
+            List<string> alist = new List<string>();
+            alist.Add("54");
+            alist.Add("HELLO");
+            Console.WriteLine("Please choose a number.");
+            string listAnswer = Console.ReadLine();
+            int listIndex = Convert.ToInt32(listAnswer);
+            if (listIndex < 0 || listIndex > 1)
+            {
+                Console.WriteLine("The data does not exist");
+            }
+            else
+            {
+                Console.WriteLine("Here is what you chose: " + alist[listIndex]);
+            }
+            
+            Console.ReadLine();
+
+          
+
+        }
+    }
+}
---
+++ ArrayAndListFollowAlong/ArrayAndListFollowAlong/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+ 
+
+namespace ArrayAndListFollowAlong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array
+            //Need to define length 
+            //Use Array when has a small fixed quantities. Difficult to change the size when set
+            //To store a very large quantities.
+            int[] numArray = new int[5]; //set the size
+            numArray[0] = 5;
+            numArray[1] = 2;
+            numArray[2] = 10;
+            numArray[3] = 200;
+            numArray[4] = 5000;
+
+            int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };//or not set a size but initialize 
+            int[] numArray2 = {5, 2, 10, 200, 5000, 600, 2300}; // just initialize
+            numArray2[5] = 650;
+
+            Console.WriteLine(numArray[3]);
+            Console.WriteLine(numArray1[3]);
+            Console.WriteLine(numArray2[3]);
+            Console.WriteLine(numArray2[5]);
+
+            //Use byte array to store binary info
+            //byte[] byteArray = new byte[5000];
+           
+            //List: create a list with any datatype. Hence, it is in the namespace Generic
+            //List can be used with any types
+            List<int> intList = new List<int>();
+            intList.Add(4);
+            intList.Add(10);
+            intList.Remove(10);
+            Console.WriteLine(intList[0]);
+
+            List<string> stringList = new List<string>();
+            stringList.Add("Hello");
+            stringList.Add("Jessie");
+            stringList.Remove("Jessie");
+            Console.WriteLine(stringList[0]);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
