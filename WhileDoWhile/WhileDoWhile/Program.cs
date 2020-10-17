dc6102df87cf0d058d9bfedb9a18a320c8b808d6
Admin <spirit2914@outlook.com>
2020-10-17 16:11:52 -0500

    Added WhileDoWhile file

--- WhileDoWhile/WhileDoWhile/Program.cs
+++ WhileDoWhile/WhileDoWhile/Program.cs
@@ -10,6 +10,84 @@ namespace WhileDoWhile
     {
         static void Main(string[] args)
         {
+            const string answer = "violet";
+            Console.WriteLine("Guess a color?");
+            string guessColor =Console.ReadLine().ToLower();
+            bool isGuess = guessColor == answer;
+            //bool isGuess = false;
+
+            do
+            {
+                switch (guessColor)//Switch without while only runs once
+                {
+                    case "blue":
+                        Console.WriteLine("You guessed blue. Try again");
+                        Console.WriteLine("Guess a color?");
+                        guessColor = Console.ReadLine().ToLower();
+                        break;
+                    case "orange":
+                        Console.WriteLine("You guessed orange. Try again");
+                        Console.WriteLine("Guess a color?");
+                        guessColor = Console.ReadLine().ToLower();
+                        break;
+                    case "green":
+                        Console.WriteLine("You guessed green. Try again");
+                        Console.WriteLine("Guess a color?");
+                        guessColor = Console.ReadLine().ToLower();
+                        break;
+                    case answer:
+                        Console.WriteLine("You guessed " + answer + ". That is correct");
+                        isGuess = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong");
+                        Console.WriteLine("Guess a color?");
+                        guessColor = Console.ReadLine().ToLower();
+                        break;
+                }
+            }
+            while (!isGuess);
+
+            //Guess a shape game
+            Console.WriteLine("");
+            const string shapeAnswer = "square";
+            Console.WriteLine("Guess a shape?");
+            string guessShape = Console.ReadLine().ToLower();
+            bool isCorrect = guessShape == shapeAnswer;
+      
+            while (!isCorrect)
+            {
+                switch (guessShape)//Switch without while only runs once
+                {
+                    case "rectangle":
+                        Console.WriteLine("You guessed rectangle. Try again");
+                        Console.WriteLine("Guess a shape?");
+                        guessShape = Console.ReadLine().ToLower();
+                        break;
+                    case "triangle":
+                        Console.WriteLine("You guessed triangle. Try again");
+                        Console.WriteLine("Guess a shape?");
+                        guessShape = Console.ReadLine().ToLower();
+                        break;
+                    case "circle":
+                        Console.WriteLine("You guessed circle. Try again");
+                        Console.WriteLine("Guess a shape?");
+                        guessShape = Console.ReadLine().ToLower();
+                        break;
+                    case shapeAnswer:
+                        Console.WriteLine("You guessed " + shapeAnswer + ". That is correct");
+                        isCorrect = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong");
+                        Console.WriteLine("Guess a shape?");
+                        guessShape = Console.ReadLine().ToLower();
+                        break;
+                }
+            }
+
+            Console.Read();
         }
     }
 }
+
