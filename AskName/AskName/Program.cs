441b21055e1cf63398a63874f4794f1248b25694
Admin <spirit2914@outlook.com>
2020-10-17 09:29:08 -0500

    Added AskName and StudentDailyProject

---
+++ AskName/AskName/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AskName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a simple program
+            //Control + K + C: add comment symbols on multiple lines
+            //Control + K + U: remove comments
+            //Console.ReadLine() only takes in string
+            //console.writeline("what is your name?");
+            //string name = console.readline();
+            //Console.writeline("your name is " + name);
+            //Console.readline();
+
+            //Console.WriteLine("What is your favorite number? ");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is " + total);
+            //Console.ReadLine();
+
+            //char questionMark = '\u2103';
+            //Console.WriteLine(questionMark);
+            //Console.ReadLine();
+
+            //decimal moneyInBank = 100.5m; //need many decimal point to avoid loosing money due to rounding
+            //double height = 21.0000000222355;
+            //float second = 2.62f;
+            //short temp = -342;//smaller than int
+
+            //Casting
+            bool isRaining = true;
+            string rainingStatus = isRaining.ToString();
+            Console.WriteLine(rainingStatus);
+
+            int currentAge = 30;
+            string yearsOld = Convert.ToString(currentAge);
+            Console.WriteLine(yearsOld);
+            Console.ReadLine();
+
+            
+
+
+        }
+    }
+}
---
+++ StudentDailyReport/StudentDailyReport/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDailyReport
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string page = Console.ReadLine();
+            Console.WriteLine("Do you need help with anything? Please answer \"true \" or \"false\".");
+            string answer = Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string share = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string hours = Console.ReadLine();
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
