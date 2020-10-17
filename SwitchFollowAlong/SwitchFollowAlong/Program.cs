e0f7e3ea85e4252c829bec5b8aeb9b245058f641
Admin <spirit2914@outlook.com>
2020-10-17 15:45:05 -0500

    Added Switch -While Follow Along

---
+++ SwitchFollowAlong/SwitchFollowAlong/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchFollowAlong
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuess = number == 12;
+            //bool isGuess = false;
+
+            do
+            {
+                switch (number)//Switch without while only runs once
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62. Try again");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29. Try again");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55. Try again");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed the number 12. That is correct");
+                        isGuess = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuess);
+                Console.Read();
+        }
+    }
+}
