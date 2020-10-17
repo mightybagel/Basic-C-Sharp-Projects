1f15183c170454b98f2e97adb11c299b5bd7c3b7
Admin <spirit2914@outlook.com>
2020-10-16 23:30:11 -0500

    Additional changes

---
+++ myConsoleProject/myConsoleProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace myConsoleProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");//Output a line of text to screen 
+            String name = Console.ReadLine();//Wait for the user's input
+            Console.WriteLine("\"Hello, " + name + "!\"");
+            Console.Read();//Wait for input
+        }
+    }
+}
+ 
