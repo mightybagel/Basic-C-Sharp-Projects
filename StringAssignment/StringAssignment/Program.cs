5a3f40b2be3ba01350fa7119ea906a4ef0100c48
Admin <spirit2914@outlook.com>
2020-10-17 17:27:27 -0500

    Added StringAssignment

---
+++ StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Append : at to the end
+            string one = "Do what you can,";
+            string second = " with what you have,";
+            string third = " where you are.";
+            string combined = one + second + third;
+            Console.WriteLine(combined.ToUpper());
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Paragraphs are the building blocks of papers. ");
+            sb.Append("Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. ");
+            sb.Append("In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph.");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringFollowAlong/StringFollowAlong/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFollowAlong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Immutable string: dynamic string object using string builder class. This relates to object and class
+            StringBuilder SB = new StringBuilder();
+            SB.Append("My name is Jessie");
+            Console.WriteLine(SB);
+            Console.ReadLine();
+
+        }
+    }
+}
