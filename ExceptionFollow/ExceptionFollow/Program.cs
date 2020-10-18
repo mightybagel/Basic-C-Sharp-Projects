452da7c52426c18444d38d6cddc46c1536446774
Admin <spirit2914@outlook.com>
2020-10-18 00:48:17 -0500

    Added try catch assignment

---
+++ ExceptionFollow/ExceptionFollow/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionFollow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Pick a number");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing two numbers");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();//Always run even before the return statement. You could use finally to log to the databse
+            }
+           
+        }
+    }
+}
---
+++ TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 10, 5, 6, 9 };
+
+            
+            try
+            {
+                Console.WriteLine("Please enter a number: ");
+                int input = Convert.ToInt32(Console.ReadLine());
+                foreach (int number in numbers)
+                {
+                    int result = number / input;
+                    Console.WriteLine(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.WriteLine("Finished try catch block");
+            Console.ReadLine();
+        }
+    }
+}
