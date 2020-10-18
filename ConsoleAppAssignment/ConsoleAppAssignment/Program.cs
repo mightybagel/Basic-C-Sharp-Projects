6302559337d201fdb0ece936bc1698c256514d7f
Admin <spirit2914@outlook.com>
2020-10-17 23:03:30 -0500

    Finished 6-part-assignment

---
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Part One
+            Console.WriteLine("Part One");
+            string[] fruits = new string[] { "apple", "kiwi", "banana" };
+            Console.WriteLine("Please enter some text: ");
+            string input = Console.ReadLine();
+            for (int i = 0; i < 3; i++)
+            {
+                fruits[i] = fruits[i] + input;
+            }
+            foreach (string text in fruits)
+            {
+                Console.WriteLine(text);
+            }
+
+            //Part Two
+            Console.WriteLine("\nPart Two");
+            int index = 0;
+            while (index < fruits.Length)
+            {
+                if (fruits[index].StartsWith("apple"))
+                {
+                    fruits[index] = "cherry";
+
+                }
+                index++;
+            }
+            foreach (string text in fruits)
+            {
+                Console.WriteLine(text);
+            }
+
+            //Part Three
+            Console.WriteLine("\nPart Three");
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine(fruits[i]);
+            }
+            Console.WriteLine("");
+            for (int i = 0; i <= 1; i++)
+            {
+                Console.WriteLine(fruits[i]);
+            }
+
+            //Part Four
+            Console.WriteLine("\nPart Four");
+            List<string> storeLists = new List<string>() { "wal-mart", "heb", "kroger", "costco" };
+            Console.WriteLine("Please enter the name of the store: ");
+            string storeName = Console.ReadLine().ToLower();
+            bool isFound = false;
+            for (int i = 0; i < storeLists.Count; i++)
+            {
+                if (storeLists[i] == storeName)
+                {
+                    Console.WriteLine("The name you entered is " + i + " index");
+                    isFound = true;
+                    break;
+                }
+            }
+            if (!isFound)
+            {
+                Console.WriteLine("Store name '" + storeName + "' is not in the list");
+            }
+
+            //Part Five
+            Console.WriteLine("\nPart Five");
+            List<string> brandLists = new List<string>() { "JBL", "APPLE", "BEAT", "APPLE" };
+            List<int> indices = new List<int>();
+            Console.WriteLine("Please enter the name of the tech brand: ");
+            string techName = Console.ReadLine().ToUpper();
+            for (int i = 0; i < brandLists.Count; i++)
+            {
+                if (brandLists[i] == techName)
+                {
+                    indices.Add(i);
+                }
+            }
+            if (indices.Count > 0)
+            {
+                Console.Write("The name you entered is found at: ");
+                foreach (int i in indices)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine(techName + " you entered is not found");
+            }
+            
+            //Part Six
+            Console.WriteLine("\nPart Six");
+            List<string> candyTypes = new List<string>() { "chocolates", "lollipops", "caramels", "gum balls", "licorices", "lollipops" };
+            List<string> appearedWords = new List<string>();
+            foreach(string type in candyTypes)
+            {
+                if (appearedWords.Contains(type))
+                {
+                    Console.WriteLine(type + " has appeared already.");
+                }
+                else
+                {
+                    Console.WriteLine(type + " has not appeared.");
+                    appearedWords.Add(type);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IterationFollowUp/IterationFollowUp/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IterationFollowUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94, };
+            for (int i = 0; i < testScores.Length; i++)
+            {
+                if (testScores[i] > 85)
+                {
+                    Console.WriteLine("Passing testscore: " + testScores[i]);
+                }
+            }
+
+            string[] names = { "Jesse", "Erik", "Daniel", "Adam" };
+            for (int j = 0; j < names.Length; j++)
+            {
+                if (names[j] == "Jesse")
+                {
+                    Console.WriteLine(names[j]);
+                }
+            }
+
+            List<int> testScores2 = new List<int>();
+            testScores2.Add(98);
+            testScores2.Add(99);
+            testScores2.Add(81);
+            testScores2.Add(72);
+            testScores2.Add(70);
+
+            foreach (int score in testScores2)//score represents the item in the list
+            {
+                if (score > 85)
+                {
+                    Console.WriteLine("Passing test score: " + score);
+                }
+            }
+
+            List<string> names2 = new List<string>() { "Jesse", "Erik", "Adam", "Daniel" };
+            foreach (string name in names2)//can't change the values of list inside foreach
+            {
+                //if(name == "Jesse")
+                Console.WriteLine(name);
+            }
+
+            List<int> testScores3 = new List<int>() { 98, 99, 12, 74, 23, 99 };
+            List<int> passingScores = new List<int>();//no need to define the size and initialize in list
+            foreach (int score in testScores3)
+            {
+                if (score > 85)
+                {
+                    passingScores.Add(score);
+                }
+            }
+            Console.WriteLine(passingScores.Count);//Count is property of list whereas array has length as property
+            Console.ReadLine();
+        }
+    }
+}
