a70b18aca429002a0d8fb00a9c067c2f5cb2e63f
Admin <spirit2914@outlook.com>
2020-10-18 15:42:41 -0500

    Corrected OptionalParameter assignment

--- OptionalParameter/OptionalParameter/Program.cs
+++ OptionalParameter/OptionalParameter/Program.cs
@@ -13,13 +13,17 @@ namespace OptionalParameter
             Calculations num = new Calculations();
             Console.WriteLine("Enter the first number: ");
             int userInput1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the second number: ");
-            int userInput2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("The total of the 1st entered number " + userInput1 + " and optional parameter 7 = " + num.Sum(userInput1));
-            Console.WriteLine("The total of " + userInput1 + " and " + userInput2 + " = " + num.Sum(userInput1,userInput2));
-            Console.WriteLine("The total of the 2nd entered number " + userInput2 + " and optional parameter 7 = " + num.Sum(userInput2));
-
+            Console.WriteLine("Enter the second number (optional; hit enter to skip): ");
+            string userInput2 = Console.ReadLine();
+            if(string.IsNullOrWhiteSpace(userInput2))
+            {
+                Console.WriteLine("The total of the 1st entered number " + userInput1 + " and optional parameter 7 = " + num.Sum(userInput1));
+            }
+            else
+            {
+                int input2 = Convert.ToInt32(userInput2);
+                Console.WriteLine("The total of " + userInput1 + " and " + userInput2 + " = " + num.Sum(userInput1, input2));
+            }
             Console.ReadLine();
         }
 
