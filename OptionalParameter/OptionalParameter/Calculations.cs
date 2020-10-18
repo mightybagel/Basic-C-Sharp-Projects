0661343de2a0394fd023aae61b0b3b1d50448013
Admin <spirit2914@outlook.com>
2020-10-18 11:38:36 -0500

    Added OptionalParameter

---
+++ OptionalParameter/OptionalParameter/Calculations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameter
+{
+    class Calculations
+    {
+        public int Sum(int num1, int num2 = 7)
+        {
+            int total = num1 + num2;
+            return total;
+        }
+    }
+}
---
+++ OptionalParameter/OptionalParameter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameter
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Calculations num = new Calculations();
+            Console.WriteLine("Enter the first number: ");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            int userInput2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The total of the 1st entered number " + userInput1 + " and optional parameter 7 = " + num.Sum(userInput1));
+            Console.WriteLine("The total of " + userInput1 + " and " + userInput2 + " = " + num.Sum(userInput1,userInput2));
+            Console.WriteLine("The total of the 2nd entered number " + userInput2 + " and optional parameter 7 = " + num.Sum(userInput2));
+
+            Console.ReadLine();
+        }
+
+    }
+}
