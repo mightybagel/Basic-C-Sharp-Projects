9723a741b69081ee632a96d6ee7a313b156763c3
Admin <spirit2914@outlook.com>
2020-10-18 11:53:07 -0500

    Added ContinuedMethod

---
+++ ContinuedMethod/ContinuedMethod/Calculations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContinuedMethod
+{
+    class Calculations
+    {
+        public void Cal(int num1, int num2)
+        {
+            int total = num1 + 25;
+            Console.WriteLine("This is the sum of 1st number " + num1 + " and 25 = " + total);
+            Console.WriteLine("This is the 2nd number " + num2);
+        }
+        
+    }
+}
---
+++ ContinuedMethod/ContinuedMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContinuedMethod
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Calculations num = new Calculations();
+            num.Cal(25, 15);
+            num.Cal(num1: 25, num2: 15);
+           
+            Console.ReadLine();
+        }
+
+    }
+}
