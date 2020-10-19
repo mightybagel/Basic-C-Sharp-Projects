6bb1118e538edc4b411dc113849e814594127644
Admin <spirit2914@outlook.com>
2020-10-18 23:57:23 -0500

    Added GenericAssignment

---
+++ GenericAssignment/GenericAssignment/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericAssignment
+{
+    public class Employee<T> : Person
+    {
+        public int Id { get; set; }
+
+        public List<T> things { get; set; }
+
+    }
+}
---
+++ GenericAssignment/GenericAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> e = new Employee<string>();
+            Employee<int> emp = new Employee<int>();
+            e.things = new List<string>() { "Jessie", "Bob", "Nick"};
+            emp.things = new List<int>() { 1, 3, 15 };
+
+            foreach(string name in e.things)
+            {
+                Console.WriteLine(name);
+            }
+            foreach (int number in emp.things)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
