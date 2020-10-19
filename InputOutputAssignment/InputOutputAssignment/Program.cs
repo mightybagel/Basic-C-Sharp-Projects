cbc55c5b08675e641fbd23098ebb55f58fb5d642
Admin <spirit2914@outlook.com>
2020-10-19 17:28:53 -0500

    Added InputOutputAssignment

---
+++ InputOutputAssignment/InputOutputAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputOutputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            string number = Console.ReadLine();
+            File.WriteAllText(@"..\..\Text\Number.txt",number);
+            string text = File.ReadAllText(@"..\..\Text\Number.txt");
+            Console.WriteLine("");
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
