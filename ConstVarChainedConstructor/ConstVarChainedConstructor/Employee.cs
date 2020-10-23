f2a81ee41a3a73e0f3aaa82e35aba403e2b3e69a
Admin <spirit2914@outlook.com>
2020-10-22 19:04:30 -0500

    AddedConstVarChainedConstrutor

---
+++ ConstVarChainedConstructor/ConstVarChainedConstructor/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVarChainedConstructor
+{
+    public class Employee
+    {
+        
+        //Chain constructor to initialize object in this case
+        public Employee() : this("Joe", "Martin")
+        {
+        }
+
+        public Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} {Id}";
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Id { get; set; }
+        
+
+    }
+}
---
+++ ConstVarChainedConstructor/ConstVarChainedConstructor/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConstVarChainedConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string title = "Employee List";
+            
+            var employee = new List<Employee>()
+            {
+                new Employee(),
+                new Employee("Lane", "Chan"),
+                new Employee("Bill", "Wells"),
+                new Employee("Joe", "Potter"),
+                new Employee("Ken", "Anderson"),
+            };
+            //new Employee() constructor for Employee employee
+            //    new Employee() { FirstName = "Jessie", LastName = "Smith" },
+            //    new Employee() { FirstName = "Bill", LastName = "Wells" },
+            //    new Employee() { FirstName = "Joe", LastName = "Potter" },
+            //    new Employee() { FirstName = "Ken", LastName = "Anderson" },
+            //    new Employee() { FirstName = "Nick", LastName = "McIntosh" },
+            //    new Employee() { FirstName = "Joe", LastName = "McDonal" },
+            //    new Employee() { FirstName = "Amy", LastName = "Ginger" },
+            //    new Employee() { FirstName = "Abby", LastName = "Tran" },
+            //    new Employee() { FirstName = "Lam", LastName = "Lee" },
+            //    new Employee() { FirstName = "Joe", LastName = "Grand" }
+            //};
+            Console.WriteLine(title);
+            foreach (Employee item in employee)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
