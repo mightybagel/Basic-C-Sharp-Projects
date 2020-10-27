ed556769af4ed13e1cd126cb7bf7a839141c883d
Admin <spirit2914@outlook.com>
2020-10-26 23:29:36 -0500

    Completed the skeleton of the admin page

--- CarInsurance/CarInsurance/Controllers/InsureeController.cs
+++ CarInsurance/CarInsurance/Controllers/InsureeController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.EnterpriseServices;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using CarInsurance.Models;
+using CarInsurance.Views.ViewModel;
 
 namespace CarInsurance.Controllers
 {
@@ -46,10 +48,14 @@ namespace CarInsurance.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType")] Insuree insuree)
         {
+
             if (ModelState.IsValid)
             {
+                decimal quote = CalculateQuote(insuree);
+                insuree.Quote = quote;
+
                 db.Insurees.Add(insuree);
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -57,6 +63,65 @@ namespace CarInsurance.Controllers
 
             return View(insuree);
         }
+
+        public static decimal CalculateQuote(Insuree insuree)
+        {
+            const decimal monthlyPayment = 50m;
+            const decimal moneyXtraAge18 = 100m;
+            const decimal moneyXtraAge1925 = 50m;
+            const decimal moneyXtra25Dollars = 25m;
+            const decimal moneyXtraForTickets = 10m;
+            decimal total = 0;
+            int insureeAge = DateTime.Now.Year - insuree.DateOfBirth.Year;
+            if (insureeAge <= 18)
+            {
+                total = monthlyPayment + moneyXtraAge18;
+            }
+            else if (insureeAge >= 19 && insureeAge <=25)
+            {
+                total = monthlyPayment + moneyXtraAge1925;
+            }
+            else if (insureeAge > 25)
+            {
+                total = monthlyPayment + moneyXtra25Dollars;
+            }
+
+            if (insuree.CarYear < 2000)
+            {
+                total += moneyXtra25Dollars;
+            }
+            else if(insuree.CarYear > 2015)
+            {
+                total += moneyXtra25Dollars;
+            }
+
+
+            if(insuree.CarMake.ToLower() == "porsche")
+            {
+                total += moneyXtra25Dollars;
+            }
+            if (insuree.CarMake.ToLower() == "porsche" && insuree.CarModel.ToLower() == "911 carrera")
+            {
+                total += moneyXtra25Dollars;
+            }
+
+            if(insuree.SpeedingTickets != 0)
+            {
+                total += (insuree.SpeedingTickets * moneyXtraForTickets);
+            }
+
+            if(insuree.DUI)
+            {
+                total += (0.25m * total);
+            }
+            if(insuree.CoverageType)
+            {
+                total += (0.5m * total);
+            }
+            return total;
+        }
+
+
 
         // GET: Insuree/Edit/5
         public ActionResult Edit(int? id)
@@ -122,6 +187,27 @@ namespace CarInsurance.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        public ActionResult Admin()
+        {
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+                List<Insuree> insurees = db.Insurees.ToList();
+                List<AdminInsureeVm> adminInsureeVms = new List<AdminInsureeVm>();
+                foreach (Insuree insuree in insurees)
+                {
+                    AdminInsureeVm adminInsureeVm = new AdminInsureeVm();
+                    adminInsureeVm.Id = insuree.Id;
+                    adminInsureeVm.FirstName = insuree.FirstName;
+                    adminInsureeVm.LastName = insuree.LastName;
+                    adminInsureeVm.EmailAddress = insuree.EmailAddress;
+                    adminInsureeVm.Quote = insuree.Quote;
+                    adminInsureeVms.Add(adminInsureeVm);
+                }
+                return View(adminInsureeVms);
+            }
+          
         }
     }
 }
---
+++ CarInsurance/CarInsurance/Views/ViewModel/AdminInsureeVm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarInsurance.Views.ViewModel
+{
+    public class AdminInsureeVm
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal Quote { get; set; }
+    }
+}
