817faa82b218073059a0bc457b8f8b67918885c6
Admin <spirit2914@outlook.com>
2020-10-29 14:14:43 -0500

    Added CodeFirstDatabaseStudent to use entity framework to create db

--- CodeFirstNewDatabaseStudents/CodeFirstNewDatabaseStudents/Program.cs
+++ CodeFirstNewDatabaseStudents/CodeFirstNewDatabaseStudents/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,24 @@ namespace CodeFirstNewDatabaseStudents
     {
         static void Main(string[] args)
         {
+            using(SchoolContext ctx = new SchoolContext())//This is the current database
+            {
+                Grade grade = new Grade()
+                {
+                    GradeName = "First"
+                };
+                ctx.Grades.Add(grade);
+
+                Student student = new Student()
+                {
+                    StudentName = "Bill Fred",
+                    DoB = DateTime.Parse("1/1/2001"),
+                    Grade = grade
+                };
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ CodeFirstNewDatabaseStudents/CodeFirstNewDatabaseStudents/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstNewDatabaseStudents
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public DateTime DoB { get; set; }
+        public Grade Grade { get; set; }
+    }
+
+
+}
